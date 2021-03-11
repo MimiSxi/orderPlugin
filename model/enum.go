7964612a192e61084e537e0d2a230c2cac161d82
MimiSxi <995499887@qq.com>
2021-03-11 18:00:13 +0800

    orderInfo定义

--- model/enum.go
+++ model/enum.go
@@ -1,29 +1,19 @@
 package model
 
-//import "github.com/Fiber-Man/funplugin"
-//
-//// 通用状态枚举类型
-//type CommonStatusEnumType uint
-//
-//const (
-//	C_ENABLE  CommonStatusEnumType = 1 // 可用
-//	C_DISABLE CommonStatusEnumType = 2 // 不可用
-//	C_DELETE  CommonStatusEnumType = 3 // 删除
-//)
-//
-//func (s CommonStatusEnumType) Enum() map[string]funplugin.EnumValue {
-//	return map[string]funplugin.EnumValue{
-//		"C_ENABLE": funplugin.EnumValue{
-//			Value:       C_ENABLE,
-//			Description: "可用",
-//		},
-//		"C_DISABLE": funplugin.EnumValue{
-//			Value:       C_DISABLE,
-//			Description: "不可用",
-//		},
-//		"C_DELETE": funplugin.EnumValue{
-//			Value:       C_DELETE,
-//			Description: "删除",
-//		},
-//	}
-//}
+import "github.com/Fiber-Man/funplugin"
+
+// 订单类型
+type OrderInfoChildrenTypeEnum uint
+
+const (
+	PHOTO_ALBUM  OrderInfoChildrenTypeEnum = 1 // 相册系统
+)
+
+func (s OrderInfoChildrenTypeEnum) Enum() map[string]funplugin.EnumValue {
+	return map[string]funplugin.EnumValue{
+		"PHOTO_ALBUM": funplugin.EnumValue{
+			Value:       PHOTO_ALBUM,
+			Description: "相册系统",
+		},
+	}
+}
---
+++ model/orderInfo.go
@@ -0,0 +1,74 @@
+package model
+
+import (
+	"errors"
+	"github.com/graphql-go/graphql"
+	"time"
+)
+
+// 订单信息
+type OrderInfo struct {
+	ID           uint                      `gorm:"primary_key" gqlschema:"query!;querys;update!" description:"订单id"`
+	ChildrenId   uint                      `gorm:"DEFAULT:0;NOT NULL;" gqlschema:"create!;querys;update;" description:"引用id"`
+	ChildrenType OrderInfoChildrenTypeEnum `gorm:"DEFAULT:0;NOT NULL;" gqlschema:"create!;querys;update;" description:"引用类型"`
+	Remark       string                    `gorm:"Type:varchar(255);DEFAULT:'';NOT NULL;" gqlschema:"create;update" description:"备注"`
+	CreatedAt    time.Time                 `description:"创建时间" gqlschema:"querys"`
+	UpdatedAt    time.Time                 `description:"更新时间" gqlschema:"querys"`
+	DeletedAt    *time.Time
+	v2           int `gorm:"-" exclude:"true"`
+}
+
+type OrderInfos struct {
+	TotalCount int
+	Edges      []OrderInfo
+}
+
+func (o OrderInfo) Query(params graphql.ResolveParams) (OrderInfo, error) {
+	p := params.Args
+	err := db.Where(p).First(&o).Error
+	return o, err
+}
+
+func (o OrderInfo) Querys(params graphql.ResolveParams) (OrderInfos, error) {
+	var result OrderInfos
+
+	dbselect := GenSelet(db, params)
+	dbcount := GenWhere(db.Model(o), params)
+
+	err := dbselect.Find(&result.Edges).Error
+	if err != nil {
+		return result, err
+	}
+	err = dbcount.Count(&result.TotalCount).Error
+	return result, err
+}
+
+func (o OrderInfo) Create(params graphql.ResolveParams) (OrderInfo, error) {
+	p := params.Args
+	o.ChildrenId = uint(p["childrenId"].(int))
+	o.ChildrenType = p["childrenType"].(OrderInfoChildrenTypeEnum)
+	if p["remark"] != nil {
+		o.Remark = p["remark"].(string)
+	}
+	err := db.Create(&o).Error
+	return o, err
+}
+
+func (o OrderInfo) Update(params graphql.ResolveParams) (OrderInfo, error) {
+	v, ok := params.Source.(OrderInfo)
+	if !ok {
+		return o, errors.New("update param")
+	}
+	p := params.Args
+	if p["childrenId"] != nil {
+		o.ChildrenId = uint(p["childrenId"].(int))
+	}
+	if p["childrenType"] != nil {
+		v.ChildrenType = p["childrenType"].(OrderInfoChildrenTypeEnum)
+	}
+	if p["remark"] != nil {
+		o.Remark = p["remark"].(string)
+	}
+	err := db.Save(&v).Error
+	return v, err
+}
